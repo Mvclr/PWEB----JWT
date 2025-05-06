9e922074dba3cd6bdfdb0bfaaafb7cf50f49c4c2
Manoel <142417832+Mvclr@users.noreply.github.com>
2025-05-06 14:23:13 -0300

    Update principal.js
    Turnind the ID re-use to an class

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,23 +1,18 @@
-import { clientes } from "./modules/clientes.js";
-import { produtos } from "./modules/produtos.js";
-
-
-
 function initializePage() {
-  let principalHtml = document.querySelector("#div-js");
+  let principalHtml = document.querySelector(".div-js");
   if (principalHtml) {
     let html = `
       <form action="/clientes" method="get">
-              <div class="card black" >${clientes.length}<br>Clientes Ativos<br><button type="submit">Gerenciar Clientes</button></div>
+              <div class="card black" >1<br>Clientes Ativos<br><button type="submit">Gerenciar Clientes</button></div>
           </form>
           <form action="/produtos"  method="get">
-              <div class="card gray">${produtos.length}<br>Produtos Cadastrados<br><button  type="submit">Gerenciar Produtos</button></div>
+              <div class="card gray">1<br>Produtos Cadastrados<br><button  type="submit">Gerenciar Produtos</button></div>
           </form>
           <div class="card yellow">44<br>Captações Pendentes<br><button>Gerenciar Captação</button></div>
     `;
     principalHtml.innerHTML = html;
   } else {
-    console.error("Element with ID 'div-js' not found in the DOM.");
+    console.error("Element with class 'div-js' not found in the DOM.");
   }
 }
 initializePage();
